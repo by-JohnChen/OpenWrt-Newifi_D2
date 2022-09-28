3a7242cb0095deb1bb867d125972c717f079af70
Devsoft <by-JohnChen@users.noreply.github.com>
2022-09-29 02:05:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@
 #=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWrt DIY script part 1 (Before Update feeds)https://github.com/by-JohnChen/OpenWrt-Newifi_D2/blob/master/diy-part1.sh
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
@@ -12,9 +12,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
